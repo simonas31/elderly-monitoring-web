7c925e58af06ee6a3d57e5c2c48c148b74667454
simonas31 <babarskas.simonas@gmail.com>
2024-04-07 20:45:37 +0300

    send alert notification

--- app/Http/Controllers/ActivityLogsController.php
+++ app/Http/Controllers/ActivityLogsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActivityLog;
+use App\Models\Device;
+use App\Models\User;
+use App\Notifications\SendFallAlertNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ActivityLogsController extends Controller
 {
@@ -11,6 +16,39 @@ class ActivityLogsController extends Controller
      */
     public function movementDetected(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'fell' => 'required|boolean',
+            'device_name' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['Check if input data is filled correctly'], 406);
+        }
+
+        $device = Device::where('device_name', $request->input('device_name'))->first();
+
+        if (!isset($device)) {
+            return response()->json(['Could not find device'], 406);
+        }
+
+        //notify the user that person fell
+        if ($request->input('fell')) {
+            $user = User::find($device->user_id);
+            $user->notify(new SendFallAlertNotification());
+
+            //find all other associated users with current user
+            $associated_users = User::where('user_id', $user->id)->get();
+            foreach ($associated_users as $associated_user) {
+                $associated_user->notify(new SendFallAlertNotification());
+            }
+        }
+
+        ActivityLog::create([
+            'device_id' => $device->id,
+            'fell' => $request->input('fell')
+        ]);
+
+        return response()->json(['OK']);
     }
 
     /**
@@ -18,5 +56,16 @@ class ActivityLogsController extends Controller
      */
     public function getStatistics(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'device_name' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['Check if input data is filled correctly'], 406);
+        }
+
+        $logs = ActivityLog::where('device_name', $request->input('device_name'))->get()->toArray();
+
+        return response()->json($logs);
     }
 }
